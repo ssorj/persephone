868ea41010633ffc1c1f742596c81b44d6f01187
Justin Ross <jross@apache.org>
2022-06-14 12:40:24 -0400

    WIP

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ curl https://raw.githubusercontent.com/ssorj/persephone/main/install.sh | sh
 ## Topics
 
  - Tries to follow the XDG base dir spec - https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
- - Default start config and subsequent steps to change the config
+ - Default start config is about evaluation
+ - Subsequent steps to change the config and open up access and add access controls
+ - Bourne shell with optional Bash extensions - *should* work on BSD and other unixes
+ - My primary target environments are Fedora, Mac OS, and Ubuntu
--- install.sh
+++ install.sh
@@ -25,13 +25,13 @@ fi
 trap "cat ~/artemis-install.log | sed 's/^/  /'" 0
 
 echo
-echo "# Checking for required tools"
+echo "# Checking for required tools" | tee -a "$LOG_FILE"
 echo
 
 # XXX Check for which and tee
 
 for tool in curl grep sed tar java; do
-    echo "-- Checking for $tool" >> "$LOG_FILE"
+    echo "## Checking for $tool" >> "$LOG_FILE"
 
     if ! which "$tool" >> "$LOG_FILE" 2>&1; then
         echo "ERROR: Required tool $tool is not available" | tee -a "$LOG_FILE"
@@ -42,17 +42,17 @@ done
 echo "  Result: OK"
 
 echo
-echo "# Downloading ActiveMQ Artemis"
+echo "# Downloading ActiveMQ Artemis" | tee -a "$LOG_FILE"
 echo
 
 (
     cd "$TEMP_DIR"
 
-    echo "-- Fetching the latest dist tarball" >> "$LOG_FILE"
+    echo "## Fetching the latest dist tarball" >> "$LOG_FILE"
 
     curl --no-progress-meter -fLo dist.tar.gz "https://www.apache.org/dyn/closer.cgi?filename=activemq/activemq-artemis/2.22.0/apache-artemis-2.22.0-bin.tar.gz&action=download" >> "$LOG_FILE" 2>&1
 
-    echo "-- Extracting files from the tarball " >> "$LOG_FILE"
+    echo "## Extracting files from the tarball " >> "$LOG_FILE"
 
     tar -xf dist.tar.gz >> "$LOG_FILE" 2>&1
     mv apache-artemis-2.22.0 dist >> "$LOG_FILE" 2>&1
@@ -61,24 +61,24 @@ echo
 echo "  Result: OK"
 
 echo
-echo "# Saving existing installation to a backup location"
+echo "# Saving existing installation to a backup location" | tee -a "$LOG_FILE"
 echo
 
-echo "-- Saving the previous config dir" >> "$LOG_FILE"
+echo "## Saving the previous config dir" >> "$LOG_FILE"
 
 if [ -e "$CONFIG_DIR" ]; then
     mkdir -p "$BACKUP_DIR/config" >> "$LOG_FILE" 2>&1
     mv "$CONFIG_DIR" "$BACKUP_DIR/config" >> "$LOG_FILE" 2>&1
 fi
 
-echo "-- Saving the previous dist dir" >> "$LOG_FILE"
+echo "## Saving the previous dist dir" >> "$LOG_FILE"
 
 if [ -e "$DIST_DIR" ]; then
     mkdir -p "$BACKUP_DIR/share" >> "$LOG_FILE" 2>&1
     mv "$DIST_DIR" "$BACKUP_DIR/share" >> "$LOG_FILE" 2>&1
 fi
 
-echo "-- Saving the previous instance dir" >> "$LOG_FILE"
+echo "## Saving the previous instance dir" >> "$LOG_FILE"
 
 if [ -e "$INSTANCE_DIR" ]; then
     mkdir -p "$BACKUP_DIR/state" >> "$LOG_FILE" 2>&1
@@ -95,15 +95,15 @@ else
 fi
 
 echo
-echo "# Installing ActiveMQ Artemis"
+echo "# Installing ActiveMQ Artemis" | tee -a "$LOG_FILE"
 echo
 
-echo "-- Moving the dist dir to its standard location" >> "$LOG_FILE"
+echo "## Moving the downloaded dist dir to its install location" >> "$LOG_FILE"
 
 mkdir -p `dirname "$DIST_DIR"`
 mv "$TEMP_DIR/dist" "$DIST_DIR"
 
-echo "-- Creating the broker instance" >> "$LOG_FILE"
+echo "## Creating the broker instance" >> "$LOG_FILE"
 
 "$DIST_DIR/bin/artemis" create "$INSTANCE_DIR" \
                         --user example --password example \
@@ -112,10 +112,13 @@ echo "-- Creating the broker instance" >> "$LOG_FILE"
                         --etc "$CONFIG_DIR" \
                         >> "$LOG_FILE" 2>&1
 
-echo "-- Burning the instance dir into the scripts" >> "$LOG_FILE"
+echo "## Burning the instance dir into the scripts" >> "$LOG_FILE"
 
-ls -l "$INSTANCE_DIR"
-ls -l "$INSTANCE_DIR/bin"
+# Cygwin fails here due to Windows drives affecting paths.  Home is on
+# C drive, and the checkout is on D drive.
+#
+# ls -l "$INSTANCE_DIR"
+# ls -l "$INSTANCE_DIR/bin"
 
 sed -i.backup "18a\\
 ARTEMIS_INSTANCE=$INSTANCE_DIR
@@ -125,7 +128,7 @@ sed -i.backup "18a\\
 ARTEMIS_INSTANCE=$INSTANCE_DIR
 " "$INSTANCE_DIR/bin/artemis-service" >> "$LOG_FILE" 2>&1
 
-echo "-- Creating symlinks to the scripts" >> "$LOG_FILE"
+echo "## Creating symlinks to the scripts" >> "$LOG_FILE"
 
 (
     mkdir -p "$BIN_DIR"
@@ -138,14 +141,14 @@ echo "-- Creating symlinks to the scripts" >> "$LOG_FILE"
 echo "  Result: OK"
 
 echo
-echo "# Testing the installation"
+echo "# Testing the installation" | tee -a "$LOG_FILE"
 echo
 
-echo "-- Testing the artemis command" >> "$LOG_FILE"
+echo "## Testing the artemis command" >> "$LOG_FILE"
 
 PATH="$BIN_DIR:$PATH" artemis version >> "$LOG_FILE" 2>&1
 
-echo "-- Checking that the required ports are available" >> "$LOG_FILE"
+echo "## Checking that the required ports are available" >> "$LOG_FILE"
 
 if which lsof > /dev/null 2>&1; then
     for port in 61616 5672 61613 1883 8161; do
@@ -156,7 +159,7 @@ if which lsof > /dev/null 2>&1; then
     done
 fi
 
-echo "-- Testing the server" >> "$LOG_FILE"
+echo "## Testing the server" >> "$LOG_FILE"
 
 PATH="$BIN_DIR:$PATH" artemis-service start >> "$LOG_FILE" 2>&1
 
@@ -182,7 +185,7 @@ kill `cat "$INSTANCE_DIR/data/artemis.pid"` >> "$LOG_FILE" 2>&1
 echo "  Result: OK"
 
 echo
-echo "# Summary"
+echo "# Summary" | tee -a "$LOG_FILE"
 echo
 
 echo "  ActiveMQ Artemis is now installed.  Use 'artemis run' to start the broker."
