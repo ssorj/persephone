eda0196440cd7be4cdad8fc93133ba9d8eee565f
Justin Ross <jross@apache.org>
2022-06-14 12:56:15 -0400

    WIP

--- install.sh
+++ install.sh
@@ -13,6 +13,21 @@ TEMP_DIR=`mktemp -d`
 BACKUP_DIR="$HOME/artemis-backup"
 LOG_FILE="$HOME/artemis-install.log"
 
+if [ -n "$BASH" ]; then
+    function trouble {
+        echo
+        echo "TROUBLE! Things didn't go to plan.  Here's the log:"
+        echo
+
+        if [ -e "$LOG_FILE" ]; then
+            cat "$LOG_FILE" | sed "s/^/  /"
+        fi
+    }
+
+    trap trouble ERR
+fi
+
+
 if [ -e "$BACKUP_DIR" ]; then
     mv "$BACKUP_DIR" "$BACKUP_DIR"-`date +%Y-%m-%d-%H-%m-%S` >> "$LOG_FILE" 2>&1
 fi
@@ -21,17 +36,14 @@ if [ -e "$LOG_FILE" ]; then
     mv "$LOG_FILE" "$LOG_FILE"-`date +%Y-%m-%d-%H-%m-%S` >> "$LOG_FILE" 2>&1
 fi
 
-# XXX
-trap "cat ~/artemis-install.log | sed 's/^/  /'" 0
-
 echo
-echo "# Checking for required tools" | tee -a "$LOG_FILE"
+echo "== Checking for required tools" | tee -a "$LOG_FILE"
 echo
 
 # XXX Check for which and tee
 
 for tool in curl grep sed tar java; do
-    echo "## Checking for $tool" >> "$LOG_FILE"
+    echo "-- Checking for $tool" >> "$LOG_FILE"
 
     if ! which "$tool" >> "$LOG_FILE" 2>&1; then
         echo "ERROR: Required tool $tool is not available" | tee -a "$LOG_FILE"
@@ -39,46 +51,46 @@ for tool in curl grep sed tar java; do
     fi
 done
 
-echo "  Result: OK"
+echo "   Result: OK"
 
 echo
-echo "# Downloading ActiveMQ Artemis" | tee -a "$LOG_FILE"
+echo "== Downloading ActiveMQ Artemis" | tee -a "$LOG_FILE"
 echo
 
 (
     cd "$TEMP_DIR"
 
-    echo "## Fetching the latest dist tarball" >> "$LOG_FILE"
+    echo "-- Fetching the latest dist tarball" >> "$LOG_FILE"
 
     curl --no-progress-meter -fLo dist.tar.gz "https://www.apache.org/dyn/closer.cgi?filename=activemq/activemq-artemis/2.22.0/apache-artemis-2.22.0-bin.tar.gz&action=download" >> "$LOG_FILE" 2>&1
 
-    echo "## Extracting files from the tarball " >> "$LOG_FILE"
+    echo "-- Extracting files from the tarball " >> "$LOG_FILE"
 
     tar -xf dist.tar.gz >> "$LOG_FILE" 2>&1
     mv apache-artemis-2.22.0 dist >> "$LOG_FILE" 2>&1
 )
 
-echo "  Result: OK"
+echo "   Result: OK"
 
 echo
-echo "# Saving existing installation to a backup location" | tee -a "$LOG_FILE"
+echo "== Saving existing installation to a backup location" | tee -a "$LOG_FILE"
 echo
 
-echo "## Saving the previous config dir" >> "$LOG_FILE"
+echo "-- Saving the previous config dir" >> "$LOG_FILE"
 
 if [ -e "$CONFIG_DIR" ]; then
     mkdir -p "$BACKUP_DIR/config" >> "$LOG_FILE" 2>&1
     mv "$CONFIG_DIR" "$BACKUP_DIR/config" >> "$LOG_FILE" 2>&1
 fi
 
-echo "## Saving the previous dist dir" >> "$LOG_FILE"
+echo "-- Saving the previous dist dir" >> "$LOG_FILE"
 
 if [ -e "$DIST_DIR" ]; then
     mkdir -p "$BACKUP_DIR/share" >> "$LOG_FILE" 2>&1
     mv "$DIST_DIR" "$BACKUP_DIR/share" >> "$LOG_FILE" 2>&1
 fi
 
-echo "## Saving the previous instance dir" >> "$LOG_FILE"
+echo "-- Saving the previous instance dir" >> "$LOG_FILE"
 
 if [ -e "$INSTANCE_DIR" ]; then
     mkdir -p "$BACKUP_DIR/state" >> "$LOG_FILE" 2>&1
@@ -88,22 +100,22 @@ fi
 # XXX Also save the scripts
 
 if [ -e "$BACKUP_DIR" ]; then
-    echo "  Result: OK"
-    echo "  Backup: $BACKUP_DIR"
+    echo "   Result: OK"
+    echo "   Backup: $BACKUP_DIR"
 else
-    echo "  Result: No existing installation"
+    echo "   Result: No existing installation"
 fi
 
 echo
-echo "# Installing ActiveMQ Artemis" | tee -a "$LOG_FILE"
+echo "== Installing ActiveMQ Artemis" | tee -a "$LOG_FILE"
 echo
 
-echo "## Moving the downloaded dist dir to its install location" >> "$LOG_FILE"
+echo "-- Moving the downloaded dist dir to its install location" >> "$LOG_FILE"
 
 mkdir -p `dirname "$DIST_DIR"`
 mv "$TEMP_DIR/dist" "$DIST_DIR"
 
-echo "## Creating the broker instance" >> "$LOG_FILE"
+echo "-- Creating the broker instance" >> "$LOG_FILE"
 
 "$DIST_DIR/bin/artemis" create "$INSTANCE_DIR" \
                         --user example --password example \
@@ -112,7 +124,7 @@ echo "## Creating the broker instance" >> "$LOG_FILE"
                         --etc "$CONFIG_DIR" \
                         >> "$LOG_FILE" 2>&1
 
-echo "## Burning the instance dir into the scripts" >> "$LOG_FILE"
+echo "-- Burning the instance dir into the scripts" >> "$LOG_FILE"
 
 # Cygwin fails here due to Windows drives affecting paths.  Home is on
 # C drive, and the checkout is on D drive.
@@ -128,7 +140,7 @@ sed -i.backup "18a\\
 ARTEMIS_INSTANCE=$INSTANCE_DIR
 " "$INSTANCE_DIR/bin/artemis-service" >> "$LOG_FILE" 2>&1
 
-echo "## Creating symlinks to the scripts" >> "$LOG_FILE"
+echo "-- Creating symlinks to the scripts" >> "$LOG_FILE"
 
 (
     mkdir -p "$BIN_DIR"
@@ -138,17 +150,17 @@ echo "## Creating symlinks to the scripts" >> "$LOG_FILE"
     ln -sf "$INSTANCE_DIR/bin/artemis-service"
 )
 
-echo "  Result: OK"
+echo "   Result: OK"
 
 echo
-echo "# Testing the installation" | tee -a "$LOG_FILE"
+echo "== Testing the installation" | tee -a "$LOG_FILE"
 echo
 
-echo "## Testing the artemis command" >> "$LOG_FILE"
+echo "-- Testing the artemis command" >> "$LOG_FILE"
 
 PATH="$BIN_DIR:$PATH" artemis version >> "$LOG_FILE" 2>&1
 
-echo "## Checking that the required ports are available" >> "$LOG_FILE"
+echo "-- Checking that the required ports are available" >> "$LOG_FILE"
 
 if which lsof > /dev/null 2>&1; then
     for port in 61616 5672 61613 1883 8161; do
@@ -159,7 +171,7 @@ if which lsof > /dev/null 2>&1; then
     done
 fi
 
-echo "## Testing the server" >> "$LOG_FILE"
+echo "-- Testing the server" >> "$LOG_FILE"
 
 PATH="$BIN_DIR:$PATH" artemis-service start >> "$LOG_FILE" 2>&1
 
@@ -182,18 +194,12 @@ PATH="$BIN_DIR:$PATH" artemis check node --verbose >> "$LOG_FILE" 2>&1
 
 kill `cat "$INSTANCE_DIR/data/artemis.pid"` >> "$LOG_FILE" 2>&1
 
-echo "  Result: OK"
+echo "   Result: OK"
 
 echo
-echo "# Summary" | tee -a "$LOG_FILE"
+echo "== Summary" | tee -a "$LOG_FILE"
 echo
 
-echo "  ActiveMQ Artemis is now installed.  Use 'artemis run' to start the broker."
-
-echo
-echo "# Log"
-echo
-
-cat ~/artemis-install.log | sed 's/^/  /'
+echo "   ActiveMQ Artemis is now installed.  Use 'artemis run' to start the broker."
 
 # XXX Path stuff!
