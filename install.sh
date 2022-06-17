98883a83ff67ea4f2dedef3748fceecbaeb236d2
Justin Ross <jross@apache.org>
2022-06-17 07:49:05 -0400

    WIP

--- install.sh
+++ install.sh
@@ -37,22 +37,22 @@ ARTEMIS_HOME_DIR="$HOME/.local/share/artemis"
 ARTEMIS_INSTANCE_DIR="$HOME/.local/state/artemis"
 
 BACKUP_DIR="$HOME/artemis-backup"
-CACHE_DIR="$HOME/.cache/artemis-install"
+CACHE_DIR="$HOME/.cache/artemis-install-script"
 LOG_FILE="$CACHE_DIR/install.log"
 
 mkdir -p "$CACHE_DIR"
 
-if [ -n "$BASH" ]; then
-    trouble() {
+trouble() {
+    if [ "$?" != 0 ]; then
         echo
         echo "TROUBLE! Things didn't go to plan. Here's the log:"
         echo
 
         cat "$LOG_FILE" || :
-    }
+    fi
+}
 
-    trap trouble ERR
-fi
+trap trouble EXIT
 
 if [ -e "$BACKUP_DIR" ]; then
     mv "$BACKUP_DIR" "$BACKUP_DIR"-`date +%Y-%m-%d-%H-%m-%S` >> "$LOG_FILE" 2>&1
@@ -62,7 +62,13 @@ if [ -e "$LOG_FILE" ]; then
     mv "$LOG_FILE" "$LOG_FILE"-`date +%Y-%m-%d-%H-%m-%S` >> "$LOG_FILE" 2>&1
 fi
 
-echo
+assert() {
+    if [ ! $1 ]; then
+        echo "ASSERTION FAILED! \"$1\"" >> "$LOG_FILE"
+        exit 1
+    fi
+}
+
 echo "== Checking for required tools" | tee -a "$LOG_FILE"
 echo
 
@@ -78,83 +84,80 @@ for tool in awk curl grep java sed sort tail tar uname; do
 done
 
 echo "   Result: OK"
-
 echo
+
 echo "== Determining the latest version" | tee -a "$LOG_FILE"
 echo
 
-VERSION=`(curl --no-progress-meter -fL https://dlcdn.apache.org/activemq/activemq-artemis/ | \
-          awk 'match($0, /[0-9]+\.[0-9]+\.[0-9]+/) { print substr($0, RSTART, RLENGTH) }' | \
-          sort -t . -k1n -k2n -k3n | \
-          tail -n 1) 2>> "$LOG_FILE"`
+VERSION=`(curl --no-progress-meter -fL https://dlcdn.apache.org/activemq/activemq-artemis/ \
+          | awk 'match($0, /[0-9]+\.[0-9]+\.[0-9]+/) { print substr($0, RSTART, RLENGTH) }' \
+          | sort -t . -k1n -k2n -k3n \
+          | tail -n 1) 2>> "$LOG_FILE"`
 
 echo "   Result: $VERSION"
-
-echo
-echo "== Downloading ActiveMQ Artemis" | tee -a "$LOG_FILE"
 echo
 
-(
-    cd "$CACHE_DIR"
+echo "== Downloading the release archive" | tee -a "$LOG_FILE"
+echo
 
-    if [ ! -e "apache-artemis-$VERSION-bin.tar.gz" ]; then
-        echo "-- Fetching the latest release tarball" >> "$LOG_FILE"
+RELEASE_ARCHIVE="$CACHE_DIR/apache-artemis-$VERSION-bin.tar.gz"
+RELEASE_DIR="$CACHE_DIR/apache-artemis-$VERSION"
 
-        curl --no-progress-meter -fLo "apache-artemis-$VERSION-bin.tar.gz" "https://www.apache.org/dyn/closer.cgi?filename=activemq/activemq-artemis/$VERSION/apache-artemis-$VERSION-bin.tar.gz&action=download" >> "$LOG_FILE" 2>&1
-    else
-        echo "-- Using the cached release tarball" >> "$LOG_FILE"
-    fi
+if [ ! -e "$RELEASE_ARCHIVE" ]; then
+    echo "-- Fetching the latest release archive" >> "$LOG_FILE"
 
-    echo "-- Extracting files from the tarball " >> "$LOG_FILE"
+    curl --no-progress-meter -fLo "$CACHE_DIR/apache-artemis-$VERSION-bin.tar.gz" "https://www.apache.org/dyn/closer.cgi?filename=activemq/activemq-artemis/$VERSION/apache-artemis-$VERSION-bin.tar.gz&action=download" >> "$LOG_FILE" 2>&1
+else
+    echo "-- Using the cached release archive" >> "$LOG_FILE"
+fi
 
-    tar -xf "apache-artemis-$VERSION-bin.tar.gz" >> "$LOG_FILE" 2>&1
-
-    # XXX We can skip this step
-    mv "apache-artemis-$VERSION" dist >> "$LOG_FILE" 2>&1
-)
+tar -C "$CACHE_DIR" -xf "$RELEASE_ARCHIVE" >> "$LOG_FILE" 2>&1
+assert "-d $RELEASE_DIR"
 
 echo "   Result: OK"
-
-echo
-echo "== Saving existing installation to a backup location" | tee -a "$LOG_FILE"
 echo
 
-echo "-- Saving the previous config dir" >> "$LOG_FILE"
+if [ -e "$ARTEMIS_CONFIG_DIR" -o -e "$ARTEMIS_HOME_DIR" -o -e "$ARTEMIS_INSTANCE_DIR" ]; then
+    echo "== Saving the existing installation to a backup" | tee -a "$LOG_FILE"
+    echo
 
-if [ -e "$ARTEMIS_CONFIG_DIR" ]; then
-    mkdir -p "$BACKUP_DIR/config" >> "$LOG_FILE" 2>&1
-    mv "$ARTEMIS_CONFIG_DIR" "$BACKUP_DIR/config" >> "$LOG_FILE" 2>&1
-fi
+    echo "-- Saving the previous config dir" >> "$LOG_FILE"
 
-echo "-- Saving the previous dist dir" >> "$LOG_FILE"
+    if [ -e "$ARTEMIS_CONFIG_DIR" ]; then
+        mkdir -p "$BACKUP_DIR/config" >> "$LOG_FILE" 2>&1
+        mv "$ARTEMIS_CONFIG_DIR" "$BACKUP_DIR/config" >> "$LOG_FILE" 2>&1
+    fi
 
-if [ -e "$ARTEMIS_HOME_DIR" ]; then
-    mkdir -p "$BACKUP_DIR/share" >> "$LOG_FILE" 2>&1
-    mv "$ARTEMIS_HOME_DIR" "$BACKUP_DIR/share" >> "$LOG_FILE" 2>&1
-fi
+    echo "-- Saving the previous dist dir" >> "$LOG_FILE"
 
-echo "-- Saving the previous instance dir" >> "$LOG_FILE"
+    if [ -e "$ARTEMIS_HOME_DIR" ]; then
+        mkdir -p "$BACKUP_DIR/share" >> "$LOG_FILE" 2>&1
+        mv "$ARTEMIS_HOME_DIR" "$BACKUP_DIR/share" >> "$LOG_FILE" 2>&1
+    fi
 
-if [ -e "$ARTEMIS_INSTANCE_DIR" ]; then
-    mkdir -p "$BACKUP_DIR/state" >> "$LOG_FILE" 2>&1
-    mv "$ARTEMIS_INSTANCE_DIR" "$BACKUP_DIR/state" >> "$LOG_FILE" 2>&1
-fi
+    echo "-- Saving the previous instance dir" >> "$LOG_FILE"
 
-if [ -e "$BACKUP_DIR" ]; then
+    if [ -e "$ARTEMIS_INSTANCE_DIR" ]; then
+        mkdir -p "$BACKUP_DIR/state" >> "$LOG_FILE" 2>&1
+        mv "$ARTEMIS_INSTANCE_DIR" "$BACKUP_DIR/state" >> "$LOG_FILE" 2>&1
+    fi
+
+    assert "-d $BACKUP_DIR"
+
     echo "   Result: OK"
     echo "   Backup: $BACKUP_DIR"
-else
-    echo "   Result: No existing installation"
+    echo
 fi
 
-echo
-echo "== Installing ActiveMQ Artemis" | tee -a "$LOG_FILE"
+echo "== Installing the broker" | tee -a "$LOG_FILE"
 echo
 
-echo "-- Moving the downloaded dist dir to its install location" >> "$LOG_FILE"
+echo "-- Moving the release dir to its install location" >> "$LOG_FILE"
+
+assert "! -e $ARTEMIS_HOME_DIR"
 
 mkdir -p `dirname "$ARTEMIS_HOME_DIR"`
-mv "$CACHE_DIR/dist" "$ARTEMIS_HOME_DIR"
+mv "$RELEASE_DIR" "$ARTEMIS_HOME_DIR"
 
 echo "-- Burning the Artemis home dir into the admin script" >> "$LOG_FILE"
 
@@ -197,6 +200,9 @@ if [ -n "$CYGWIN" ]; then
 
     echo "-- Patching problem 2" >> "$LOG_FILE"
 
+    # And this bit replaces a colon with a semicolon in the
+    # bootclasspath.  Windows requires a semicolon.
+
     sed -i.backup3 's/\$LOG_MANAGER:\$WILDFLY_COMMON/\$LOG_MANAGER;\$WILDFLY_COMMON/' "$ARTEMIS_INSTANCE_DIR/bin/artemis"
 fi
 
@@ -211,8 +217,8 @@ echo "-- Creating symlinks to the scripts" >> "$LOG_FILE"
 )
 
 echo "   Result: OK"
-
 echo
+
 echo "== Testing the installation" | tee -a "$LOG_FILE"
 echo
 
@@ -255,8 +261,8 @@ fi
 kill `cat "$ARTEMIS_INSTANCE_DIR/data/artemis.pid"` >> "$LOG_FILE" 2>&1
 
 echo "   Result: OK"
-
 echo
+
 echo "== Summary" | tee -a "$LOG_FILE"
 echo
 
