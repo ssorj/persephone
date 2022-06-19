4c92a05feb859bf99a04101ade2e214f3a586715
Justin Ross <jross@apache.org>
2022-06-19 08:01:32 -0400

    WIP

--- install.sh
+++ install.sh
@@ -257,29 +257,29 @@ ARTEMIS_INSTANCE=${artemis_instance_dir}
 ARTEMIS_INSTANCE=${artemis_instance_dir}
 " "${artemis_instance_dir}/bin/artemis-service"
 
-        # if [ -n "${cygwin:-}" ]
-        # then
-        #     log "Patching problem 1"
+        if [ -n "${cygwin:-}" ]
+        then
+            # log "Patching problem 1"
 
-        #     # This bit of the Artemis instance script uses a cygpath --unix,
-        #     # cygpath --windows sequence that ends up stripping out the drive
-        #     # letter and replacing it with whatever the current drive is. If your
-        #     # current drive is different from the Artemis install drive, trouble.
-        #     #
-        #     # For the bug: Annotate the current code.  Suggest --absolute.
+            # # This bit of the Artemis instance script uses a cygpath --unix,
+            # # cygpath --windows sequence that ends up stripping out the drive
+            # # letter and replacing it with whatever the current drive is. If your
+            # # current drive is different from the Artemis install drive, trouble.
+            # #
+            # # For the bug: Annotate the current code.  Suggest --absolute.
 
-        #     # XXX Try patching for --absolute instead
+            # # XXX Try patching for --absolute instead
 
-        #     sed -i.backup2 "77,82d" "${artemis_instance_dir}/bin/artemis"
+            # sed -i.backup2 "77,82d" "${artemis_instance_dir}/bin/artemis"
 
-        #     log "Patching problem 2"
+            log "Patching problem 2"
 
-        #     # And this bit replaces a colon with a semicolon in the
-        #     # bootclasspath.  Windows requires a semicolon.
+            # And this bit replaces a colon with a semicolon in the
+            # bootclasspath.  Windows requires a semicolon.
 
-        #     # shellcheck disable=SC2016 # I don't want these expanded
-        #     sed -i.backup3 's/\$LOG_MANAGER:\$WILDFLY_COMMON/\$LOG_MANAGER;\$WILDFLY_COMMON/' "${artemis_instance_dir}/bin/artemis"
-        # fi
+            # shellcheck disable=SC2016 # I don't want these expanded
+            sed -i.backup3 's/\$LOG_MANAGER:\$WILDFLY_COMMON/\$LOG_MANAGER;\$WILDFLY_COMMON/' "${artemis_instance_dir}/bin/artemis"
+        fi
 
         log "Creating symlinks to the scripts"
 
