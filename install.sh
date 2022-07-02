9b0cf4b2c8d8ee430f72179cc6445ff0017810e5
Justin Ross <jross@apache.org>
2022-07-02 09:44:25 -0400

    WIP

--- install.sh
+++ install.sh
@@ -525,7 +525,7 @@ generate_password() {
     assert program_is_available tr
     assert program_is_available head
 
-    head -c 1024 /dev/urandom | LC_ALL=C tr -dc 'a-z0-9' | head -c 8
+    head -c 1024 /dev/urandom | LC_ALL=C tr -dc 'a-z0-9' | head -c 16
 }
 
 #
@@ -809,7 +809,6 @@ main() {
         print "    Version:           ${release_version}"
         print "    Config files:      ${artemis_config_dir}"
         print "    Log files:         ${artemis_instance_dir}/log"
-        print "    Data files:        ${artemis_instance_dir}/data"
         print "    Console user:      example"
         print "    Console password:  ${password}"
 
